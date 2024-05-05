83f5252f59b7d40a2d91614139daec0873935ca6
DedSerg <168771309+DedSerg@users.noreply.github.com>
2024-05-05 10:15:25 +0300

    Add files via upload

---
+++ sunday.py
@@ -0,0 +1,10 @@
+name = 'это моя строка'
+print(name[0])
+print(name[13:])
+print(name[2:5])
+print(name[::-1])
+print(len(name))
+name = 'это моя строка,'
+kiks = 'это новая строка'
+print(name + kiks)
+
