79e8e2647ce68c7296960b756c0289c30435c966
DedSerg <168771309+DedSerg@users.noreply.github.com>
2024-05-09 21:40:40 +0300

    Add files via upload

--- homework3.py
+++ homework3.py
@@ -1,8 +1,9 @@
-#my_string = input("Добрый вечер, ")
+# my_string = input("Добрый вечер, ")
+# print(my_string)
 my_string = "Добрый вечер, господа"
 print(len(my_string))
 print(my_string.upper())
 print(my_string.lower())
 print(my_string.replace( " " , ""))
-print(my_string[1:])
-print(my_string[:20])
+print(my_string[0])
+print(my_string[:-1])
