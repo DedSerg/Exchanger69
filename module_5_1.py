59b9a78ad36368b13fe52324235b76da9d8f8af5
DedSerg <168771309+DedSerg@users.noreply.github.com>
2024-06-21 21:17:29 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -4,10 +4,12 @@ class House():
         self.number_of_floors = number_of_floors
     def go_to(self, new_floor):
         if new_floor < 1 or new_floor > self.number_of_floors:
-            print(f'В {self.name} этажа {new_floor} не существует')
+             print(f'В {self.name} этажа {new_floor} не существует')
         else:
+            for i in range(1, new_floor + 1):
+                print(i)
             print(f'Приветствуем в {self.name}, поехали на {new_floor} этаж')
 gk_1 = House('ЖК "Эльбрус"', 30)
 gk_2 = House('ЖК "Завидное"', 15)
-print(gk_1.go_to(int(input())))
-print(gk_2.go_to(int(input())))
+gk_1.go_to(int(input()))
+gk_2.go_to(int(input()))
