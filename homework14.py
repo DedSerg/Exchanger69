c8d4a21b5d3ac0620a1ba15973c5642e08f5ef32
DedSerg <168771309+DedSerg@users.noreply.github.com>
2024-06-24 23:47:28 +0300

    Add files via upload

--- homework14.py
+++ homework14.py
@@ -1,7 +1,11 @@
 class Buiding:
     def __init__(self, total):
-        self.total = total
-buiding = [Buiding(f'Здание {i+1}') for i in range (40)]
+
+         buiding = [Buiding(f'Здание {i + 1}') for i in range(40)]
 for i in range(40):
-    print(f'Дом {i+1}')
+    print(f'Дом {i + 1}')
+
+
+
+
 
