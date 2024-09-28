83ac1c2aff67864d9ddfa8fa50ec14c0059cba3b
DedSerg <168771309+DedSerg@users.noreply.github.com>
2024-09-28 20:06:37 +0300

    Add files via upload

---
+++ module_12_1.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from runner import Runner
+
+
+class RunnerTest(TestCase):
+    def test_walk(self):
+        r1 = Runner('Гоша')
+        for _ in range(10):
+            r1.walk()
+        self.assertEqual(r1.distance, 50, f'{r1.name} пробежал {r1.distance}, а должен был 50')
+
+    def test_run(self):
+        r2 = Runner('Сёма')
+        for _ in range(10):
+            r2.run()
+        self.assertEqual(r2.distance, 100, f'{r2.name} пробежал {r2.distance}, а должен был 100')
+
+    def test_challenge(self):
+        r1 = Runner('Гоша')
+        r2 = Runner('Сёма')
+        for _ in range(10):
+            r1.walk()
+            r2.run()
+        self.assertNotEqual(r1.distance, r2.distance)
