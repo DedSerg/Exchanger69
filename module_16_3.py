7e9f60d193030696ebc7da7247eb2f8ec789949f
DedSerg <168771309+DedSerg@users.noreply.github.com>
2024-11-22 12:30:50 +0300

    Add files via upload

---
+++ module_16_3.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+users = {'1': 'Имя: Example, возраст: 18'}
+
+@app.get("/")
+async def get_main_page():
+    return "Главная страница"
+
+@app.get("/users")
+async def get_all_dict():
+    return users
+
+
+@app.post("/user/{username}/{age}")
+async def create_user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example='UrbanUser')],
+                      age: Annotated[int, Path(ge=18, le=120, description="Enter age", example='24')]):
+    user_id = str(int(max(users, key=int)) + 1) # Генерируем новый ID пользователя, увеличивая максимальный
+    # существующий ID на 1.
+    users[user_id] = f"Имя: {username}, возраст: {age}" # Добавляем нового пользователя в словарь users
+    return f"User {user_id} is registered"
+
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(user_id: Annotated[str, Path(ge=1, le=100, description='Enter user id', example='1')],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example='UrbanProfi')],
+                      age: Annotated[int, Path(ge=18, le=120, description="Enter age", example='28')]):
+    users[user_id] = f"Имя: {username}, возраст: {age}" # обновляются данные существующего пользователя
+    # по указанному user_id
+    return f"User {user_id} has been updated"
+
+
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter user id', example='1')]):
+    users.pop(str(user_id))
+    return f"User {user_id} has been deleted"
